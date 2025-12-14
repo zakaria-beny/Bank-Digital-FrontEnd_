551b43d6fda27ce051fec73b7ac3b1aa93b3b12f
zasbira <natsoighnile@gmail.com>
2025-12-14 20:41:07 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Clients } from './clients/clients';
+import { Comptes } from './comptes/comptes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'clients', component: Clients },
+  { path: 'comptes', component: Comptes },
+  { path: '', redirectTo: 'clients', pathMatch: 'full' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from './nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, NavBar],
+  template: `
+    <app-nav-bar></app-nav-bar>
+    <router-outlet></router-outlet>
+  `
 })
-export class App {
-  protected readonly title = signal('Bank-Digital-FrontEnd');
-}
+export class App {}
