bd7940db3cea559e1743d1ca1e3ffb2c01c9aa17
zasbira <natsoighnile@gmail.com>
2025-12-20 00:33:06 +0100

    motcle

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideZoneChangeDetection } from '@angular/core';
+
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
--- src/app/clients/clients.ts
+++ src/app/clients/clients.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ClientsSerivces } from '../services/clients-serivces';
+import {FormBuilder, FormGroup } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-clients',
   standalone: true,
+  imports: [CommonModule, HttpClientModule,ReactiveFormsModule],
   templateUrl: './clients.html',
-  styleUrl: './clients.css'
+  styleUrls: ['./clients.css']
 })
-export class Clients {}
+export class Clients implements OnInit {
+
+  clients: any ;
+  searchformGroup!: FormGroup ;
+  constructor(private clientservice: ClientsSerivces ,private fb:FormBuilder) {}
+
+  ngOnInit(): void {
+    this.searchformGroup=this.fb.group(
+      { motcle :this.fb.control("")}
+
+    )
+    this.clientservice.getclients().subscribe({
+      next: (data) => {
+        this.clients = data;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  protected handleSearchClients() {
+
+
+  }
+}
