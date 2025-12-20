f7956809ef84ded79b84298a6db291e44aa43d03
zasbira <natsoighnile@gmail.com>
2025-12-21 00:56:27 +0100

    lundi commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Clients } from './clients/clients';
 import { Comptes } from './comptes/comptes';
+import {NoveauClient} from'./noveau-client/noveau-client';
 
 export const routes: Routes = [
   { path: 'clients', component: Clients },
   { path: 'comptes', component: Comptes },
+  { path: 'noveau-client', component: NoveauClient },
   { path: '', redirectTo: 'clients', pathMatch: 'full' }
 ];
